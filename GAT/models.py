edc913e1c8414a1479273dcf7c5b1260b9b56105
william <william@williamde-MacBook-Pro-2.local>
2020-03-19 16:55:17 +0800

    [MODIFY] output logits directly

--- GAT/models.py
+++ GAT/models.py
@@ -37,7 +37,8 @@ class GCN(nn.Module):
         x = F.dropout(x, p=self.dropout, training=self.training)
         x = self.conv2(x, edge_index)
 
-        return F.log_softmax(x, dim=1)
+        # return F.log_softmax(x, dim=1)
+        return x
 
 
 class GATConv(MessagePassing):
@@ -157,7 +158,8 @@ class GAT(nn.Module):
         x = F.dropout(x, p=self.input_dropout, training=self.training)
         x = self.conv2(x, edge_index)
 
-        return F.log_softmax(x, dim=1)
+        # return F.log_softmax(x, dim=1)
+        return x
 
 
 class GATBlock(nn.Module):
@@ -230,4 +232,5 @@ class MultiGAT(nn.Module):
         x = self.dropout(x)
         x = self.conv(x, edge_index)
 
-        return F.log_softmax(x, dim=1)
+        # return F.log_softmax(x, dim=1)
+        return x
